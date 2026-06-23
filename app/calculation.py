f5067de1196b2d0ed1349428ad8bbce75bc3c939
Sophia Luinenburg <videogamefpslover@gmail.com>
2026-06-23 17:09:01 -0400

    Long calculations handled through a linked list

---
+++ app/calculation.py
@@ -0,0 +1,80 @@
+from __future__ import annotations
+
+import sys
+
+class Operation:
+    POWER = "^"
+    ROOT = "%"
+    MULTIPLY = "*"
+    DIVIDE = "/"
+    ADD = "+"
+    SUBTRACT = "-"
+    END = "="
+
+class OperationLink:
+    def __init__(self, number: float, operation: Operation):
+        self.number = number
+        self.operation = operation
+        self.next = None
+    def AssignOperation(self, operation: Operation):
+        self.operation = operation
+    def AssignNext(self, next_link: OperationLink):
+        self.next = next_link
+    def __str__(self):
+        link = self
+        val = ""
+        while link is not None:
+            if link.operation == Operation.END:
+                val += f"{link.number}"
+                break
+            val += f"{link.number} {link.operation} "
+            link = link.next
+        return val
+
+class Calculator:
+    def __init__(self):
+        self.head = None
+        self.result = None
+
+    def calculate(self):
+        if not self.head:
+            raise ValueError("No calculation to perform.")
+
+        current_link = self.head
+        while current_link is not None and current_link.operation != Operation.END:
+            if current_link.operation == Operation.POWER:
+                current_link.number **= current_link.next.number
+                current_link.AssignNext(current_link.next.next)
+            elif current_link.operation == Operation.ROOT:
+                if current_link.next.number == 0:
+                    raise ZeroDivisionError("Division by zero.")
+                current_link.number **= 1 / current_link.next.number
+                current_link.AssignNext(current_link.next.next)
+            current_link = current_link.next
+        current_link = self.head
+        
+        while current_link is not None and current_link.operation != Operation.END:
+            if current_link.operation == Operation.MULTIPLY:
+                current_link.number *= current_link.next.number
+                current_link.AssignNext(current_link.next.next)
+            elif current_link.operation == Operation.DIVIDE:
+                if current_link.next.number == 0:
+                    raise ZeroDivisionError("Division by zero.")
+                current_link.number /= current_link.next.number
+                current_link.AssignNext(current_link.next.next)
+            current_link = current_link.next
+        current_link = self.head
+        
+        while current_link is not None and current_link.operation != Operation.END:
+            if current_link.operation == Operation.ADD:
+                current_link.number += current_link.next.number
+                current_link.AssignNext(current_link.next.next)
+            elif current_link.operation == Operation.SUBTRACT:
+                if current_link.next.number == 0:
+                    raise ZeroDivisionError("Division by zero.")
+                current_link.number -= current_link.next.number
+                current_link.AssignNext(current_link.next.next)
+            current_link = current_link.next
+        
+        self.result = self.head.number
+        return self.result
